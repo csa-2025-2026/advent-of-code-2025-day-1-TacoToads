c15b6c46bed05227da6aa3e0ae2dcee485e66bf3
TacoToads <enhminecraft@gmail.com>
2025-12-18 19:21:48 +0000

    i

--- Main.java
+++ Main.java
@@ -2,47 +2,59 @@ import java.io.IOException;
 import java.io.File;
 import java.util.Scanner;
 
+
 public class Main
 {
   public static void main(String[] args) throws IOException
   {
-    File file = new File(input.txt);  // this is your problem input as a File object
+    File file = new File("input.txt");  // this is your problem input as a File object
     Scanner sc = new Scanner(file);   // the Scanner object is now reading from the file object
 
 
+    int count = 0;
     int answer = 50;
     while (sc.hasNext())  // as long as the Scanner has more lines to read from the file...
     {
       String line = sc.nextLine();  // gets the next line of input.  This is like "R802"
 
-      // TODO: Write code that gets the turning direction from the line.  This should be "L" or "R"
 
+      // TODO: Write code that gets the turning direction from the line.  This should be "L" or "R"
       String direction = line.substring(0,1);
 
+
       // TODO: Write code that gets a substring of the line that contains only the numbers
-
-      Sting turns = line.substring(1);
-
       // For R802, this would be a String variable with the value "802"
+      String num = line.substring(1);
+
 
       // TODO: Using the parseInt method from the Integer class, convert the String "802" into an int
+      int x = Integer.parseInt(num);
 
-      int x = Integer.parseInt(turns);
-      )
 
       // TODO: Now that you have the turning direction, and the turning amount, see if the turn results in a 0
-      if (direction == "L")
+      if (direction.equals("L"))
       {
-        answer -= turns;
+        answer -= x;
       }
       else
       {
-        answer += turns;
+        answer += x;
       }
 
+
+      if (answer % 100 == 0)
+      {
+        count ++;
+      }
     }
 
-    System.out.println("The password is " + answer);
+
+
+
+    System.out.println("The password is " + count);
     sc.close();
   }
 }
+
+
+
